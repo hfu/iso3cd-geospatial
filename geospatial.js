f21736792f74491eee05a1e5437a6e24c36c4f39
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-03-07 14:34:58 -0500

    update

---
+++ geospatial.js
@@ -0,0 +1,113 @@
+function loadScript (src, callback) {
+  var done = false
+  var head = document.getElementsByTagName('head')[0]
+  var script = document.createElement('script')
+  script.onload = script.onreadlystatechange = function () {
+    if (
+      !done &&
+      (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')
+    ) {
+      done = true
+      callback()
+      script.onload = script.onreadystatechange = null
+      if (head && script.parentNode) { head.removeChild(script) }
+    }
+  }
+  script.src = src
+  head.appendChild(script)
+}
+
+var clientUrl = document.location.href.split('?')[1]
+console.log('loading ' + clientUrl)
+
+loadScript(clientUrl, function () {
+  console.log('yay')
+
+  expression = ['match', ['get', 'iso3cd']]
+  cartotiles.data.forEach(function (row) {
+    expression.push(row[0], cartotiles.color(row[1]))
+  })
+  expression.push(['rgb', 224, 224, 224])
+
+  style = {
+    'version': 8,
+    'sprite': 'https://hfu.github.io/unite-sprite/sprite',
+    'glyphs': 'https://vectortiles.xyz/fonts/{fontstack}/{range}.pbf',
+    'sources': {
+      'iso3cd': {
+        'type': 'geojson',
+        'data': 'https://hfu.github.io/iso3cd-scaffold/scaffold.geojson'
+      }
+    },
+    'layers': [
+      {
+        'id': 'iso3cd-circle',
+        'type': 'circle',
+        'source': 'iso3cd',
+        'layout': {},
+        'paint': {
+          'circle-radius': [
+            'interpolate',
+            [
+              'exponential',
+              2
+            ],
+            [
+              'zoom'
+            ],
+            0,
+            4,
+            6,
+            400
+          ],
+          'circle-color': expression
+        }
+      },
+      {
+        'id': 'iso3cd-symbol',
+        'type': 'symbol',
+        'source': 'iso3cd',
+        'minzoom': 2,
+        'layout': {
+          'text-font': [
+            'sans'
+          ],
+          'text-field': [
+            'step',
+            [
+              'zoom'
+            ],
+            [
+              'get',
+              'iso3cd'
+            ],
+            4,
+            [
+              'get',
+              'name'
+            ]
+          ]
+        },
+        'paint': {
+          'text-color': [
+            'rgb',
+            255,
+            255,
+            255
+          ]
+        }
+      }
+    ]
+  }
+
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: style,
+    attributionControl: true,
+    hash: true
+  })
+
+  map.on('load', function () {
+    map.addControl(new mapboxgl.NavigationControl())
+  })
+})
